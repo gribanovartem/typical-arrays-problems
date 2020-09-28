9277ee8f70d2fc59084ea5959ec73ad2633ffef9
gribanovartem <gribanovartem22@gmail.com>
2020-09-28 21:18:35 +0300

    feat: implement 'Typical Arrays Problems'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  if(array.length !== 0) {
+    return Math.min.apply(null, array);
+  } else return 0;
+  
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  if(array.length !== 0) {
+    return Math.max.apply(null, array);
+  } else return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  if(array.length !== 0) {
+    return array.reduce((acc, item, i) => {
+      return acc + item;
+    }, 0) / array.length;
+  } else return 0;
 }
